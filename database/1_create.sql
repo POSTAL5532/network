81f412c6dfc2002f66053f1738eb89a72fd4e6c0
Scherbachenya, Igor <evgeniionegin791@gmail.com>
2022-02-17 15:56:24 +0300

    Change configs for services. Added messages and posts tables.

--- database/1_create.sql
+++ database/1_create.sql
@@ -9,3 +9,25 @@ CREATE TABLE users
     confirm_status VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE messages
+(
+    id               VARCHAR(36)  NOT NULL,
+    from_user_id     VARCHAR(36)  NOT NULL,
+    to_user_id       VARCHAR(36)  NOT NULL,
+    text             TEXT         NOT NULL,
+    sent_date        TIMESTAMP    NOT NULL,
+    delivered_status VARCHAR(255) NOT NULL,
+    read_status      VARCHAR(255) NOT NULL,
+    confirm_status   VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE post
+(
+    id        VARCHAR(36) NOT NULL,
+    user_id   VARCHAR(36) NOT NULL,
+    text      TEXT        NOT NULL,
+    post_date TIMESTAMP   NOT NULL,
+    PRIMARY KEY (id)
+);
